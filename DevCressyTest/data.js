c3acaaba0becf75826774a878548ac17f10196d6
DevCressy <81008794+DevCressy@users.noreply.github.com>
2021-03-20 15:56:06 +0200

    Add files via upload

---
+++ DevCressyTest/data.js
@@ -0,0 +1,72 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360-1",
+      "name": "360 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.826497140895924,
+          "pitch": 0.06991926013905214,
+          "rotation": 0,
+          "target": "1-y-360-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-y-360-1",
+      "name": "Y 360 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
